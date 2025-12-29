3177668254a76686f7b52bbfb421b1692ffc3c92
snhossain <snhossain.8765@gmail.com>
2025-12-29 12:11:07 +0000

    Add files via upload

---
+++ Calculator Project.py
@@ -0,0 +1,24 @@
+#The defining stage
+
+operations = {
+    "add": lambda a,b: a + b,
+    "subtract": lambda a,b: a - b,
+    "divide": lambda a,b: a / b,
+    "multiply": lambda a,b: a * b,
+    "exponent": lambda a,b: a ** b,
+    "average": lambda a,b: add(a, b)/2
+}
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+q = input("What would you like to do with a and b in its respective order?").lower()
+
+#Computational and output stage
+
+if q in operations:
+    r = operations[q](a,b)
+else:
+    r = ("Syntax error - try again!")
+
+print(r)
